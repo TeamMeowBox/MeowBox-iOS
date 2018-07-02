4e90c58d5102cb98d4eeb923509390cc252b0bb9
장용범 <dnwkqk@gmail.com>
2018-07-02 13:49:03 +0900

    add orderwithinfo 2,3,4, complete view

--- MeowBox/MeowBox/JangDta/OrderWithInfo2ViewController.swift
+++ MeowBox/MeowBox/JangDta/OrderWithInfo2ViewController.swift
@@ -16,20 +16,11 @@ class OrderWithInfo2ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func goWithInfo3(_ sender: Any) {
+        let vc = UIStoryboard(name: "Order", bundle: nil).instantiateViewController(withIdentifier: "OrderWithInfo3ViewController") as! OrderWithInfo3ViewController
+        
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
