b356253377584aa4158397df41c15b624069a36e
장용범 <dnwkqk@gmail.com>
2018-07-03 00:07:50 +0900

    add order branch first

--- MeowBox/MeowBox/JangDta/OrderWithInfo3ViewController.swift
+++ MeowBox/MeowBox/JangDta/OrderWithInfo3ViewController.swift
@@ -16,20 +16,12 @@ class OrderWithInfo3ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func goWithInfo4(_ sender: Any) {
+        let vc = UIStoryboard(name: "Order", bundle: nil).instantiateViewController(withIdentifier: "OrderWithInfo4ViewController") as! OrderWithInfo4ViewController
+        
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
