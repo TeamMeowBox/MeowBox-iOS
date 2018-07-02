427640d7a7b59e6564b37aa87b3021d6990c878a
kyeahen <bright19961@naver.com>
2018-07-02 23:23:55 +0900

    Update Sign.storyboard, LoginVC, MainVC, SignInVC

--- MeowBox/MeowBox/Kyeahen/ViewController/LoginViewController.swift
+++ MeowBox/MeowBox/Kyeahen/ViewController/LoginViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-//TODO: 이미지뷰 로고 및 텍스트 필드 아이콘 넣기
-//TODO: 네비게이션바 투명한거 불투명으로 바꾸기
+//TODO: 이미지뷰 로고
+//TODO: 입력 내용 채워지면 아이콘들 핑크색 아이콘으로 변경
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailImageView: UIImageView!
+    @IBOutlet weak var pwdImageView: UIImageView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //navigation bar title color
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : #colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.3137254902, alpha: 1)]
 
+        emptyCheck(tf: emailTextField, iv: emailImageView, icon1: #imageLiteral(resourceName: "email-icon-pink"), icon2: #imageLiteral(resourceName: "email-icon-gray"))
+        emptyCheck(tf: pwdTextField, iv: pwdImageView, icon1: #imageLiteral(resourceName: "password-icon-pink"), icon2: #imageLiteral(resourceName: "password-icon-gray"))
         // Do any additional setup after loading the view.
     }
 
@@ -22,7 +33,21 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func emptyCheck(tf: UITextField, iv: UIImageView ,icon1: UIImage, icon2: UIImage) {
+        if tf.text == ""{
+            iv.image = icon2
+        }
+        
+       // tf.addTarget(self, action:d Selector(("textChanged:")), for: .editingChanged)
+    }
+    
+    //MARK: 뒤로가기 액션
+    @IBAction func dismissAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: 회원가입 액션
     @IBAction func signInAction(_ sender: Any) {
         let signInNaviVC = UIStoryboard(name: "Sign", bundle: nil).instantiateViewController(withIdentifier: "SignInNaviVC")
         
@@ -30,3 +55,22 @@ class LoginViewController: UIViewController {
     }
     
 }
+
+//extension UITextField {
+//    func modifyClearButton(with image : UIImage) {
+//        let icon = UIImage()
+//
+//
+//        clearButton.setImage(image, for: .normal)
+//        clearButton.frame = CGRect(x: 0, y: 0, width: 15, height: 15)
+//        clearButton.contentMode = .scaleAspectFit
+//        clearButton.addTarget(self, action: #selector(UITextField.clear(_:)), for: .touchUpInside)
+//        rightView = clearButton
+//        rightViewMode = .whileEditing
+//    }
+//
+//    func clear(_ sender : AnyObject) {
+//        self.text = ""
+//        sendActions(for: .editingChanged)
+//    }
+//}
