ac3a99e29b2ade07f2ad04f8d7b5e580077a2106
장용범 <dnwkqk@gmail.com>
2018-07-05 16:04:37 +0900

    add main collection view paging

---
+++ MeowBox/MeowBox/JangDta/MainCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MainCollectionViewCell.swift
+//  MeowBox
+//
+//  Created by 장한솔 on 2018. 7. 5..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+}
