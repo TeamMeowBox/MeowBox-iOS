e7ca37644df24fffeb26b22d28cbef63cd20a8e7
장용범 <dnwkqk@gmail.com>
2018-07-05 17:54:19 +0900

    add ticket page 1

---
+++ MeowBox/MeowBox/JangDta/TicketTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TicketTableViewCell.swift
+//  MeowBox
+//
+//  Created by 장한솔 on 2018. 7. 5..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import UIKit
+
+class TicketTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ticketView: UIView!
+    @IBOutlet weak var ticketNameLabel: UILabel!
+    @IBOutlet weak var ticketTermLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
