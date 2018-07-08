82742782642e965e715bcdd2d03d83184e1826c4
kyeahen <bright19961@naver.com>
2018-07-08 18:24:06 +0900

    mypage ticket X - server sucess

---
+++ MeowBox/MeowBox/Kyeahen/Util/Model/MyPageNoneTicket.swift
@@ -0,0 +1,15 @@
+//
+//  MyPageNoneTicket.swift
+//  MeowBox
+//
+//  Created by 김예은 on 2018. 7. 8..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import Foundation
+
+struct MyPageNoneTicket: Codable {
+    let catinfo: String
+    let flag: String
+    let sendImage: String
+}
---
+++ MeowBox/MeowBox/Kyeahen/Util/Model/MyPageNoneTicketData.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageNoneTicketData.swift
+//  MeowBox
+//
+//  Created by 김예은 on 2018. 7. 8..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import Foundation
+
+struct MyPageNoneTicketData: Codable {
+    let message: String
+    let result: MyPageNoneTicket
+}
