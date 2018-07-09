beada808cbadbad0094db0fa9b0d136e9c87ca29
kyeahen <bright19961@naver.com>
2018-07-10 04:12:00 +0900

    add server catCount

---
+++ MeowBox/MeowBox/Kyeahen/Util/Service/MainService.swift
@@ -0,0 +1,75 @@
+//
+//  MainService.swift
+//  MeowBox
+//
+//  Created by 김예은 on 2018. 7. 10..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+struct MainService: APIService {
+    
+    //MARK: 미유박스 이야기 고양이 수
+    static func catCountInit(completion: @escaping (CatCountData)-> Void) {
+        
+        let URL = url("/home/monthlyBox_detail/catCount")
+        
+        let userdefault = UserDefaults.standard
+        guard let token = userdefault.string(forKey: "token") else { return }
+        
+        let token_header = [ "authorization" : token ]
+        
+        Alamofire.request(URL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: token_header).responseData() { res in
+            
+            switch res.result {
+                
+            case .success:
+                
+                if let value = res.result.value {
+                    
+                    print("cat 접근")
+                    let decoder = JSONDecoder()
+                    
+                    do {
+                        print("cat 진입")
+                        
+                        let catCountData = try decoder.decode(CatCountData.self, from: value)
+                        
+                        if catCountData.status == true {
+                            
+                            let message = JSON(value)["message"].string
+                            print(message)
+                            if catCountData.message == "success" {
+                                print("cat 성공")
+                                completion(catCountData)
+                            }
+                            else {
+                                print("cat 실패")
+                            }
+                        }
+                            
+                        else {
+                            print("서버 에러")
+                        }
+                        
+                    } catch {
+                        print("예외 발생")
+                        
+                    }
+                    
+                }
+                break
+                
+            case .failure(let err):
+                print(err.localizedDescription)
+                break
+            }
+        }
+        
+    }
+    
+    
+}
