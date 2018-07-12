a888efdd731ae161d622d9ee729a498f086dde1d
kyeahen <bright19961@naver.com>
2018-07-13 07:48:36 +0900

    Update webtoon, birthBox, detail

--- MeowBox/MeowBox/Kyeahen/ViewController/BirthTableViewCell.swift
+++ MeowBox/MeowBox/Kyeahen/ViewController/BirthTableViewCell.swift
@@ -16,7 +16,88 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
     @IBOutlet weak var tablePageControl: UIPageControl!
     
     var currentPage = 0
-    var imageArr = [#imageLiteral(resourceName: "birthday-background-img.png"), #imageLiteral(resourceName: "baking-img.png"), #imageLiteral(resourceName: "birthday-period-img.png")]
+
+    
+    let ribbonArr = [#imageLiteral(resourceName: "ribbon-two-img"), #imageLiteral(resourceName: "ribbon-three-img"), #imageLiteral(resourceName: "ribbon-four-img.png")]
+    let cakeArr = [#imageLiteral(resourceName: "cake-two-img"), #imageLiteral(resourceName: "cake-three-img"), #imageLiteral(resourceName: "cake-four-img")]
+    let toyArr = [#imageLiteral(resourceName: "birthday-toy-one-img"), #imageLiteral(resourceName: "birthday-toy-two-img"), #imageLiteral(resourceName: "birthday-toy-three-img")]
+    let scArr = [#imageLiteral(resourceName: "scratcher-two-img"), #imageLiteral(resourceName: "scratcher-three-img.png"), #imageLiteral(resourceName: "scratcher-four-img.png")]
+    let snackArr = [#imageLiteral(resourceName: "snack-two-img"),#imageLiteral(resourceName: "snack-three-img.png"), #imageLiteral(resourceName: "snack-four-img.png")]
+    
+    let ribbonTextArr = [
+        """
+        생일 축하해요! 이 모자와 리본을 착용한 순간,
+        오늘의 주인공은 바로 당신!
+        """,
+        """
+        귀여운 금색 고깔모자와 리본타이는
+        당신의 고양이의 품격을 올려줄 거예요.
+        """,
+        """
+        두 제품 모두 조절 끈이 있어,
+        우리 냥이에게 꼭 맞게 착용할 수 있어요.
+        """
+    ]
+    
+    let cakeTextArr = [
+        """
+        함께 기뻐하기를 바라는 마음을 가득 담아,
+        냥이가 먹을 수 있는 특별한 케이크를 만들었어요
+        """,
+        """
+        우리 냥이만을 위한 맞춤 가랜드로
+        더욱 특별한 추억을 남겨보세요.
+        """,
+        """
+        일 년에 한 번, 특별한 우리만의 날을 기념할
+        케이크와 가랜드!
+        """
+    ]
+    
+    let toyTextArr = [
+        """
+        냥이와 케미 폭발할 아기자기한 인형,
+        가지고 놀기 딱 좋은 사이즈로 만들었어요.
+        """,
+        """
+        세상 부드러운 양모볼에 캣닢가루를 솔솔~
+        하루 종일 갖고 놀아도 질리지 않아요.
+        """,
+        """
+        다양한 귀여운 인형들도 있어요.
+        랜덤으로 두개씩 보내드려요!
+        """
+    ]
+    
+    let scTextArr = [
+        """
+        스트레스를 해소해 줄 스크래쳐.
+        한 번 긁으면, 멈출 수 없을 걸?
+        """,
+        """
+        영양소를 그대로 담은 동결건조 닭가슴살,
+        맛과 건강 둘 다 놓치지 않을 거예요!
+        """,
+        """
+        미유박스와 딱 맞는 크기의 스크래쳐,
+        합체하면 아늑한 냥이만의 공간 완성!
+        """
+    ]
+    
+    let snackTextArr = [
+        """
+        영양소를 그대로 담은 동결건조 닭가슴살,
+        맛과 건강 둘 다 놓치지 않을 거예요!
+        """,
+        """
+        까다로운 냥이도 무너뜨릴 향기,
+        갈매기살을 갈아 만들어 말랑한 식감의 포크 져키
+        """,
+        """
+        미유가 만들고 수의사가 검사한 믿음직한
+        수제간식, 영양소와 기호성 또한 최고!
+        """
+    ]
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -38,7 +119,7 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArr.count
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -46,12 +127,8 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
         if tag == 0 {
             if let cell: BirthCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: BirthCollectionViewCell.reuseIdentifier, for: indexPath) as? BirthCollectionViewCell
             {
-                cell.collectionImageView.image = imageArr[indexPath.row]
-                cell.collectionLabel.text =
-                """
-                생일 축하해요! 이 모자와 리본을 착용한 순간,
-                오늘의 주인공은 바로 당신!
-                """
+                cell.collectionImageView.image = ribbonArr[indexPath.row]
+                cell.collectionLabel.text = ribbonTextArr[indexPath.row]
             
                 
                 return cell
@@ -62,12 +139,8 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
             
             if let cell: BirthCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: BirthCollectionViewCell.reuseIdentifier, for: indexPath) as? BirthCollectionViewCell
             {
-                cell.collectionImageView.image = imageArr[indexPath.row]
-                cell.collectionLabel.text =
-                """
-                함께 기뻐하기를 바라는 마음을 가득 담아,
-                냥이가 먹을 수 있는 특별한 케이크를 만들었어요
-                """
+                cell.collectionImageView.image = cakeArr[indexPath.row]
+                cell.collectionLabel.text = cakeTextArr[indexPath.row]
             
                 return cell
             }
@@ -77,12 +150,8 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
             
             if let cell: BirthCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: BirthCollectionViewCell.reuseIdentifier, for: indexPath) as? BirthCollectionViewCell
             {
-                cell.collectionImageView.image = imageArr[indexPath.row]
-                cell.collectionLabel.text =
-                """
-                냥이와 케미 폭발할 아기자기한 인형,
-                가지고 놀기 딱 좋은 사이즈로 만들었어요.
-                """
+                cell.collectionImageView.image = toyArr[indexPath.row]
+                cell.collectionLabel.text = toyTextArr[indexPath.row]
                 
                 return cell
             }
@@ -91,12 +160,8 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
         else if tag == 3 {
             if let cell: BirthCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: BirthCollectionViewCell.reuseIdentifier, for: indexPath) as? BirthCollectionViewCell
             {
-                cell.collectionImageView.image = imageArr[indexPath.row]
-                cell.collectionLabel.text =
-                """
-                스트레스를 해소해 줄 스크래쳐.
-                한 번 긁으면, 멈출 수 없을 걸?
-                """
+                cell.collectionImageView.image = scArr[indexPath.row]
+                cell.collectionLabel.text = scTextArr[indexPath.row]
                 
                 return cell
             }
@@ -105,12 +170,8 @@ class BirthTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
         else {
             if let cell: BirthCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: BirthCollectionViewCell.reuseIdentifier, for: indexPath) as? BirthCollectionViewCell
             {
-                cell.collectionImageView.image = imageArr[indexPath.row]
-                cell.collectionLabel.text =
-                """
-                영양소를 그대로 담은 동결건조 닭가슴살,
-                맛과 건강 둘 다 놓치지 않을 거예요!
-                """
+                cell.collectionImageView.image = snackArr[indexPath.row]
+                cell.collectionLabel.text = snackTextArr[indexPath.row]
                 
                 return cell
             }
--- MeowBox/MeowBox/Kyeahen/ViewController/MeowBoxDetailTableViewCell.swift
+++ MeowBox/MeowBox/Kyeahen/ViewController/MeowBoxDetailTableViewCell.swift
@@ -18,7 +18,88 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
     var currentPages = 0
     
     
-    let collectImageArr = [#imageLiteral(resourceName: "package-box-img"),#imageLiteral(resourceName: "package-box-detail-img"),#imageLiteral(resourceName: "package-box-img")]
+    let sunglassArr = [#imageLiteral(resourceName: "sunglass-two-img.png"), #imageLiteral(resourceName: "sunglass-three-img.png"), #imageLiteral(resourceName: "sunglass-four-img.png")]
+    let toyArr = [#imageLiteral(resourceName: "toy-two-img.png"), #imageLiteral(resourceName: "toy-three-img.png"), #imageLiteral(resourceName: "toy-four-img.png")]
+    let fishArr = [#imageLiteral(resourceName: "fishing-rod-two-img"), #imageLiteral(resourceName: "fishing-rod-three-img.png"), #imageLiteral(resourceName: "fishing-rod-four-img")]
+    let scArr = [#imageLiteral(resourceName: "scratcher-two-img"), #imageLiteral(resourceName: "scratcher-three-img.png"), #imageLiteral(resourceName: "scratcher-four-img.png")]
+    let snackArr = [#imageLiteral(resourceName: "snack-two-img"),#imageLiteral(resourceName: "snack-three-img.png"), #imageLiteral(resourceName: "snack-four-img.png")]
+    
+    let sungTextArr = [
+        """
+        2018 S/S 피서룩의 완성.
+        선글라스와 모자만 있으면, 이구역 힙냥이는 나야!
+        """,
+        """
+        마음대로 길이조절이 가능한 밀짚모자!
+        우리 냥이에게 딱 맞게 씌워주세요.
+        """,
+        """
+        볼수록 더 귀여운 밀짚모자와 선글라스.
+        냥이에게 멋진 피서를 선물하는 법, 어렵지 않아요!
+        """
+    ]
+    
+    let toyTextArr = [
+        """
+        보기만 해도 시원해!
+        냥이가 씹고 뜯고 맛보고 즐길 올여름 최애 인형
+        """,
+        """
+        세상 부드러운 양모볼에 캣닢가루를 솔솔~
+        하루 종일 갖고 놀아도 질리지 않아요.
+        """,
+        """
+        냥이와 케미 폭발할 아기자기한 인형,
+        가지고 놀기 딱 좋은 사이즈로 만들었어요.
+        """
+    ]
+    
+    let fishTextArr = [
+        """
+        숨어있던 사냥본능을 깨워라!
+        자유자재 길이조절로 늘 새로운 3단 낚시대
+        """,
+        """
+        길고 부드러운 깃털과 방울 소리로
+        냥이의 사냥본능을 자극해요.
+        """,
+        """
+        엉키지 않는 긴 낚시줄과 길이 조절이 가능한 낚시대로
+        더 재밌고 편하게 놀아줄 수 있어요.
+        """
+    ]
+    
+    let scTextArr = [
+        """
+        스트레스를 해소해 줄 스크래쳐.
+        한 번 긁으면, 멈출 수 없을 걸?
+        """,
+        """
+        영양소를 그대로 담은 동결건조 닭가슴살,
+        맛과 건강 둘 다 놓치지 않을 거예요!
+        """,
+        """
+        미유박스와 딱 맞는 크기의 스크래쳐,
+        합체하면 아늑한 냥이만의 공간 완성!
+        """
+    ]
+    
+    let snackTextArr = [
+        """
+        영양소를 그대로 담은 동결건조 닭가슴살,
+        맛과 건강 둘 다 놓치지 않을 거예요!
+        """,
+        """
+        까다로운 냥이도 무너뜨릴 향기,
+        갈매기살을 갈아 만들어 말랑한 식감의 포크 져키
+        """,
+        """
+        미유가 만들고 수의사가 검사한 믿음직한
+        수제간식, 영양소와 기호성 또한 최고!
+        """
+    ]
+    
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -43,7 +124,7 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return collectImageArr.count
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -51,9 +132,8 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
         if tag == 0 {
             if let cell: MeowBoxDetailCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeowBoxDetailCollectionViewCell", for: indexPath) as? MeowBoxDetailCollectionViewCell
             {
-            cell.collectionImageView.image = collectImageArr[indexPath.row]
-            cell.collectionLabel.text = "시원해지는 원단"
-
+            cell.collectionImageView.image = sunglassArr[indexPath.row]
+            cell.collectionLabel.text = sungTextArr[indexPath.row]
             
             return cell
             }
@@ -62,8 +142,8 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
         else if tag == 1 {
             if let cell: MeowBoxDetailCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeowBoxDetailCollectionViewCell", for: indexPath) as? MeowBoxDetailCollectionViewCell
             {
-                cell.collectionImageView.image = collectImageArr[indexPath.row]
-                cell.collectionLabel.text = "시원해지는 원단"
+                cell.collectionImageView.image = toyArr[indexPath.row]
+                cell.collectionLabel.text = toyTextArr[indexPath.row]
                 
                 
                 return cell
@@ -73,8 +153,8 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
         else if tag == 2 {
             if let cell: MeowBoxDetailCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeowBoxDetailCollectionViewCell", for: indexPath) as? MeowBoxDetailCollectionViewCell
             {
-                cell.collectionImageView.image = collectImageArr[indexPath.row]
-                cell.collectionLabel.text = "시원해지는 원단"
+                cell.collectionImageView.image = fishArr[indexPath.row]
+                cell.collectionLabel.text = fishTextArr[indexPath.row]
                 
                 
                 return cell
@@ -84,8 +164,8 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
         else if tag == 3 {
             if let cell: MeowBoxDetailCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeowBoxDetailCollectionViewCell", for: indexPath) as? MeowBoxDetailCollectionViewCell
             {
-                cell.collectionImageView.image = collectImageArr[indexPath.row]
-                cell.collectionLabel.text = "시원해지는 원단"
+                cell.collectionImageView.image = scArr[indexPath.row]
+                cell.collectionLabel.text = scTextArr[indexPath.row]
                 
                 
                 return cell
@@ -95,8 +175,8 @@ class MeowBoxDetailTableViewCell: UITableViewCell,UICollectionViewDataSource, UI
         else {
             if let cell: MeowBoxDetailCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeowBoxDetailCollectionViewCell", for: indexPath) as? MeowBoxDetailCollectionViewCell
             {
-                cell.collectionImageView.image = collectImageArr[indexPath.row]
-                cell.collectionLabel.text = "시원해지는 원단"
+                cell.collectionImageView.image = snackArr[indexPath.row]
+                cell.collectionLabel.text = snackTextArr[indexPath.row]
                 
                 
                 return cell
